15d746715295bf7e84ecf591e33d3dbf653f3665
Thef1nansist <vladsavhchenko994@gmail.com>
2021-09-04 16:08:57 +0300

    Lab1 Acquaintance

---
+++ Labs1/js.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+let h = (r) => {
+    let rc = '';
+    for(key in r.headers) 
+    rc += '<h3>' + key + ':' + r.headers[key] + '</h3>';
+    return rc;
+
+}
+
+http.createServer(function (request, response){
+  let b = '';
+  request.on('data', str=>{
+      b+= str;
+      console.log('data',b);
+  })
+  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+  request.on('end', ()=> 
+  response.end(
+      '<body>'+
+      '<h1>Структура Документа</h1>' +
+      '<h2>'+ 'метод:' + request.method + '</h2>'+
+      '<h2>'+ 'uri:' + request.url + '</h2>'+
+      '<h2>'+ 'версия:' + request.httpVersion + '</h2>'+
+      '<h2>'+ 'Заголовки:' + '</h2>'+
+      h(request)+
+      '<h2>'+ 'тело:' + b + '</h2>'+
+      '</body>'
+
+  ))
+}).listen(3000);
+
+console.log('Server running')
