ff8577b67b784986dc5a68d00bf72381beeb568a
Thef1nansist <vladsavhchenko994@gmail.com>
2021-10-14 02:00:40 +0300

    Norm Labs + Added still few Bulma css fraemwork

---
+++ Labs4/04-01.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var data = require('./DB');
+
+var db = new data.DB;
+
+db.on('GET', (req, resp) => {
+    console.log('DB.get');
+    resp.end(JSON.stringify(db.select()));
+});
+
+db.on('POST', (req, resp) => {
+    console.log('DB.post');
+    req.on('data', data => {
+        let res = JSON.parse(data);
+        res.id = Number(res.id);
+        db.insert(res);
+        resp.end(JSON.stringify(data));
+    });
+});
+
+db.on('PUT', (req, resp) => {
+    console.log("DB.put");
+    req.on('data', data => {
+        let res = JSON.parse(data);
+        let flag = db.update(res);
+
+        resp.end(flag ? 'completed' : 'not completed');
+    });
+});
+db.on('DELETE', (req, resp) => {
+    console.log("DB.delete");
+
+    let queryData = url.parse(req.url, true).query;
+    let id = queryData.id;
+    let line = db.delete(id);
+    console.log(line);
+    resp.end(line !== undefined ? JSON.stringify(line) : 'Selected element was not found');
+});
+
+http.createServer(function (request, response) {
+
+    if (url.parse(request.url).pathname === '/api/db') {
+        db.emit(request.method, request, response);
+    }
+}).listen(3000);
+
---
+++ Labs4/DB.js
@@ -0,0 +1,50 @@
+var util = require('util');
+var ee = require('events');
+
+var db_data = [
+    { 'id': 1, 'name': 'Savchenko V. Y.', 'bday': '2001-05-31' },
+    { 'id': 2, 'name': 'Shiman D. V.', 'bday': '1956-05-31' },
+    { 'id': 3, 'name': 'Putin V. V.', 'bday': '1968-05-31' }
+];
+
+function DB() {
+    this.select = () => {
+        return db_data;
+    };
+
+    this.insert = (request) => {
+        let index = db_data.findIndex(x => x.id === Number(request.id))
+        console.log(index);
+        console.log(db_data);
+        if (index === -1) {
+            db_data.push(request);
+        }
+    };
+
+    this.update = (newEl) => {
+        console.log(newEl.id);
+        let index = db_data.findIndex(x => x.id === Number(newEl.id));
+        console.log(index);
+        if (index !== -1) {
+            db_data[index] = newEl;
+
+            return true;
+        }
+
+        return false;
+    };
+
+    this.delete = (id) => {
+        let index = db_data.findIndex(x => x.id === Number(id));
+        console.log(index);
+        if (index !== -1) {
+            let elemArr = db_data[index];
+            console.log(elemArr);
+            db_data.splice(index, 1);
+            return elemArr;
+        }
+    }
+}
+
+util.inherits(DB, ee.EventEmitter);
+exports.DB = DB;
